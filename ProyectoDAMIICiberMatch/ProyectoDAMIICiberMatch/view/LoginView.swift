662f11d49135c633abbd513bdaadbd0b28553188
Jans110718 <jansyaredjade111@gmail.com>
2024-12-03 08:30:47 -0500

    Register

--- ProyectoDAMIICiberMatch/ProyectoDAMIICiberMatch/view/LoginView.swift
+++ ProyectoDAMIICiberMatch/ProyectoDAMIICiberMatch/view/LoginView.swift
@@ -41,11 +41,18 @@ struct LoginView: View {
                 // Redirección si el usuario inicia sesión
                 NavigationLink(
                     destination: SalaView()
-                    .navigationBarBackButtonHidden(true), // Oculta la flecha de regreso
-
+                        .navigationBarBackButtonHidden(true), // Oculta la flecha de regreso
                     isActive: $isLoggedIn
                 ) {
                     EmptyView()
+                }
+
+                // Opción de redirección a la vista de registro
+                HStack {
+                    Text("¿No tienes cuenta?")
+                    NavigationLink("Registrarse", destination: RegisterView()) // Aquí agregamos el enlace a la vista de registro
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
                 }
 
                 Spacer()
@@ -69,7 +76,7 @@ struct LoginView: View {
             if let error = error {
                 errorMessage = error.localizedDescription
             } else {
-                isLoggedIn = true // Esto activa la navegación a MovieListView
+                isLoggedIn = true // Esto activa la navegación a SalaView
                 print("Usuario inició sesión: \(String(describing: result?.user.email))")
             }
         }
